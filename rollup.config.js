c587666d19cef194ce5584b2d441ca64f036cd0b
jesus.javega <jesusgraficap@gmail.com>
2019-02-21 16:02:23 +0100

    config prettier

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+const json = require("rollup-plugin-json");
+const commonjs = require("rollup-plugin-commonjs");
+const resolve = require("rollup-plugin-node-resolve");
+const babel = require("rollup-plugin-babel");
+const nodeBuiltIns = require("rollup-plugin-node-builtins");
+const nodeGlobals = require("rollup-plugin-node-globals");
+const uglifier = require("rollup-plugin-uglify");
+
+module.exports = {
+  input: "src/index.js",
+  output: {
+    file: "dist/index.js",
+    format: "umd",
+    name: "apiClient",
+    globals: {
+      "lodash.clonedeep": "cloneDeep",
+      axios: "axios",
+      "axios-retry": "axiosRetry"
+    }
+  },
+  external: ["axios", "axios-retry", "lodash.clonedeep"],
+  plugins: [
+    resolve({
+      module: true,
+      main: true,
+      browser: true,
+      jsnext: true,
+      preferBuiltins: true
+    }),
+    commonjs({
+      include: "node_modules/**"
+    }),
+    json(),
+    babel(),
+    nodeBuiltIns(),
+    nodeGlobals(),
+    uglifier.uglify()
+  ]
+};
